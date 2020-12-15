e58e9eb4b67fec5e26a882505388db6c34265277
Nate <nathaniel.reister@gmail.com>
2020-12-15 06:47:26 -0800

    add.commit files

---
+++ C#-Projects/C#-Coding-Projects/README.md
@@ -0,0 +1,16 @@
+<dl>
+  <dt>C# Projects</dt>
+  <dt>C# Coding Projects:</dt>
+  <dd>These are examples of both guided and self made programs utilizing C#. They include:
+</dd>
+
+<dt>C# Coding Projects:</dt>
+<dd>These are examples of both guided and self made programs utilizing C#. Further Description in a README in the folder.
+</dd>
+
+<dt>Basic C# Projects:</dt>
+<dd>This is a collection of various exercise that I have completed durning my schooling. While this does not showcase all of the 
+exercises that I completed this is a select few I have enjoyed or showcase some principle aspects of C#.
+</dd>
+
+</dl>
---
+++ C#-Projects/C#-Coding-Projects/TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+
+//using System.IO;
+
+namespace TwentyOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //IO/Logging
+            //string text = "Here is some text.";
+            //File.WriteAllText(@"C:\Users\natha\Logs\logs.txt", text);
+            //string text = File.ReadAllText(@"C:\Users\natha\Logs\logs.txt");
+
+            //DateTime
+            //DateTime yearOfBirth = new DateTime(1995, 5, 26, 8, 32, 45);
+            //DateTime yearOfGraduation = new DateTime(2013, 6, 1, 16, 34, 22);
+            //TimeSpan ageAtGraduation = yearOfGraduation - yearOfBirth;
+
+            const string casinoName = "Grand Hotel and Casino"; //use of a constant
+
+            // Guid identifier = Guid.NewGuid();  creation of a Guid
+
+            Console.WriteLine("Welcome to the {0}! Let's start by telling me your name.", casinoName);
+            string playerName = Console.ReadLine();
+            if (playerName.ToLower() == "admin")
+            {
+                List<ExceptionEntity> Exceptions = ReadExceptions();//creates a list of all exceptions logged in the db
+                foreach (var exception in Exceptions)
+                {
+                    Console.Write(exception.Id + " | ");
+                    Console.Write(exception.ExceptionType + " | ");
+                    Console.Write(exception.ExceptionMessage + " | ");
+                    Console.Write(exception.TimeStamp + " | ");
+                    Console.WriteLine();
+                }
+                Console.Read();
+                return;
+            }
+
+            bool validAnswer = false; //handled exception. All text bellow.
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer) Console.WriteLine("Please enter digits only, no decimals.");
+            }
+            
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                player.Id = Guid.NewGuid();
+                using (StreamWriter file = new StreamWriter(@"C:\Users\natha\Logs\logs.txt", true)) //logging the Guid
+                {
+                    file.WriteLine(player.Id);
+                }
+                Game game = new TwentyOneGame();
+                game += player;
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException ex)
+                    {
+                        Console.WriteLine(ex.Message);//the message assigned to this exception can be used here as well.
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("An error ocurred. Please contact your system Administrator.");
+                        UpdateDbWithException(ex);
+                        Console.ReadLine();
+                        return;
+                    }
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now.");
+            Console.Read();
+
+        }
+        private static void UpdateDbWithException(Exception ex)
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;
+                                        Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                        TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                        MultiSubnetFailover=False"; //connection string
+            string queryString = @"INSERT INTO Exceptions (ExceptionType, ExceptionMessage, TimeStamp) VALUES
+                                    (@ExceptionType, @ExceptionMessage, @TimeStamp)";//query and parameterized queries(second line)
+            using (SqlConnection connection = new SqlConnection(connectionString))//using is a way of controling unmanaged code/resources and protecting from injections
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                command.Parameters.Add("@ExceptionType", SqlDbType.VarChar);//whats being added
+                command.Parameters.Add("@ExceptionMessage", SqlDbType.VarChar);
+                command.Parameters.Add("@TimeStamp", SqlDbType.VarChar);
+
+                command.Parameters["@ExceptionType"].Value = ex.GetType().ToString();//values to be passed in
+                command.Parameters["@ExceptionMessage"].Value = ex.Message;
+                command.Parameters["@TimeStamp"].Value = DateTime.Now;
+
+                connection.Open();
+                command.ExecuteNonQuery();
+                connection.Close();
+            }
+        }
+        private static List<ExceptionEntity> ReadExceptions()  //returns a list of exception entities
+        {
+            string connectionString = @"Data Source=(localdb)\ProjectsV13;Initial Catalog=TwentyOneGame;
+                                        Integrated Security=True;Connect Timeout=30;Encrypt=False;
+                                        TrustServerCertificate=False;ApplicationIntent=ReadWrite;
+                                        MultiSubnetFailover=False"; //connection string
+            string queryString = @"Select ID, ExceptionType, ExceptionMessage, TimeStamp From Exceptions";
+
+            List<ExceptionEntity> Exceptions = new List<ExceptionEntity>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                SqlCommand command = new SqlCommand(queryString, connection);
+                connection.Open();
+
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    ExceptionEntity exception = new ExceptionEntity();
+                    exception.Id = Convert.ToInt32(reader["Id"]);
+                    exception.ExceptionType = reader["ExceptionType"].ToString();
+                    exception.ExceptionMessage = reader["ExceptionMessage"].ToString();
+                    exception.TimeStamp = Convert.ToDateTime(reader["TimeStamp"]);
+                    Exceptions.Add(exception);
+                }
+                connection.Close();
+            }
+            return Exceptions;
+        }
+    }
+}
